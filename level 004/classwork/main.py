987a5fba3cda0a9fea67d8aa1b3c649b7d3e4a79
GabrielLebanidze <gabriellebanidze@gmail.com>
2025-07-12 21:33:18 +0400

    fourth commit

---
+++ level 004/classwork/main.py
@@ -0,0 +1,11 @@
+name = "gugugaga"
+print(name * 8)
+# კომენტარი არის ტექსტი რომელსაც კომპიუტერი აიგნორებს. გამოიყენება სხვებისთვის ინსტრუქციების მისაცემად, კოდის განმარტებისთვის.
+num1 = 10
+num2 = 1.9
+num3 = num1 * num2
+print(num3 * num3)
+
+name1 = " Gabriel "
+surname = " Lebanidze"
+print("Hello, my name is" + name1 + "and my surname is" + surname )
---
+++ level 004/homework/main.py
@@ -0,0 +1,8 @@
+# debugging ნიშნავს პროგრამირებაში error-ების, ანუ შეცდომების გასწორებას.
+# კონკადინაცია არის string-ის string-ზე დამატება. მაგ: print("pluh" + "huh"), print("gugugaga" + "rel"), print("lel" + "bike")
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+num5 = 5
+print((num1 + num2 + num3 + num4 + num5)/5)
