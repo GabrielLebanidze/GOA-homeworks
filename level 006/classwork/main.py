c5fad32bcf05052227bde184398e556c32c1ed17
GabrielLebanidze <gabriellebanidze@gmail.com>
2025-07-16 17:22:29 +0400

    sixth commit

---
+++ level 006/classwork/main.py
@@ -0,0 +1,8 @@
+# input არის ჩვენ მიერ მიცემული ინფორმაცია კომპიუტერისთვის. input device მაგალითია: კლავიატურა, მაუსი, მიკროფონი და ა. შ.
+# output არის კომპიუტერის მიერ გამოტანილი ინფორმაცია. output-ის მაგალითია: ეკრანი, ტერმინალი, დინამიკები და ა. შ.
+name = input("enter your name here")
+surname = input("enter your surname here")
+age = input("enter your age here")
+print("Hello, my name is " + name)
+print("My surname is " + surname)
+print("I am " + age)
---
+++ level 006/homework/main.py
@@ -0,0 +1,4 @@
+# snake case არის ქვედატირის, _-ის დაწერა, როდესაც ცვლადში მრავალ სიტყვას ვიყენებთ. მაგ: hello_world.
+num1 = input("enter your number")
+num2 = input("enter your number")
+print(num1 + num2)
