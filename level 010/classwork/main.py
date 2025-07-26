e901cd24982a0caa094ee4e90a7da93a852302bd
GabrielLebanidze <gabriellebanidze@gmail.com>
2025-07-26 16:00:36 +0400

    tenth commit

---
+++ level 010/classwork/main.py
@@ -0,0 +1,27 @@
+# explicity ნიშნავს ტექსტის ტიპის შეცვლას ჩვენს მიერ. მაგ: print(int(type(10.0))
+# implicity ნიშნავს ტექსტის ტიპის შეცვლას კომპიუტერის მიერ აუტომატურად. მაგ: print(10 + 10.0)
+name = "Gabriel "
+age = 14
+print("hi, my name is " + name + "im " + str(age))
+print(int(input("enter your favorite number here")) **(5))
+# if არის ბრძანება, რომლის გამოყენების შემთხვევაში შეგიძლია მოახდინო ფუნქციები, თუ მაგალითად პირველი ცვლადი მეორეზე მეტია, დაწერე რომ პირველი მეტია მეორეზე. მაგ:
+# e = 50
+# r = 20
+# if e > r:
+#   print("e is bigger than r")
+# elif არის ბრძანება, რომელიც გამოიყენება იმ შემთხევაში, როცა if ბრძანება არ არის სწორი. elif კი კიდევ ერთხელ ცდის. მაგ:
+# e = 50
+# r = 20
+# if e < r:
+#   print("e is less than r")
+# elif e > r:
+#   print("e is bigger than r")
+# else არის ბრძანება, რომელიც ასწორებს if-ს და elif-ს. შეგიძლია მხოლოდ if შეასწორო else-ით. მაგ:
+# e = 50
+# r = 20
+# if e < r:
+#   print("e is less than r")
+# elif e == r:
+#   print("e equals r")
+# else:
+#   print("e is bigger than r")
---
+++ level 010/homework/main.py
@@ -0,0 +1,4 @@
+# data conversion ნიშნავს რაიმე data ტიპის შეცვლას ფუნქციებით: int(), str(), float()
+# int() ფუნქცია string-ს და float-ს გადააკეთებს integer-ად. მაგ: int("13"), int(10.5), int("29"). 
+# str() ფუნქცია integer-ს და float-ს გადააკეთებს string-ად, ანუ ბრჭყალებში ჩასვამს. მაგ: str(10), str(1.5), str(1234).
+# float() ფუნქცია string-ს და integer-ს გადააკეთებს float-ად. მაგ: float(1), float("23"), float(1234).
