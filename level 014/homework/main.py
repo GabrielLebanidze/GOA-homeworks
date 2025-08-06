a1d91a89a3617840381817af6c626f5fe01c89ab
GabrielLebanidze <gabriellebanidze@gmail.com>
2025-08-07 00:21:19 +0400

    fourteenth commit\

---
+++ level 014/homework/main.py
@@ -0,0 +1,18 @@
+# ალგორითმი არის რამე, რასაც აქვს ინსტრუქციები.
+# sequencing ნიშნავს, იმას, რომ კომპიუტერი ჩვენს მიერ დაწერილ კოდს. ზევიდან ქვევით, თანმიმდევრობით კითხულობს.
+# iteration არის loop-ის შემადგენელი ნაწილი, რამეს გამეორების ერთი ცალკე პროცესი.
+# selection ნიშნავს, კომპიუტერის მიერ მოქმედების არჩევა. მაგ: თუ შედეგი არის True, უნდა მოხდეს ეს, თუ False, უნდა მოხდეს ის.
+# flowchart არის ალგორითმის ჩანახატი, რომ ადვილად დავინახოთ თუ როგორ მუშაობს ის.
+# pseudo code არის პროგრამირების ენასთან მიახლოებული ჩანაწერი, რომ ადვილად გავიგოთ ალგორითმი.
+# natural language-ს იყენებენ ალგორითმის დამწყებები და, რომ გაუადვილდეთ სწავლა ჩვეულებრივ ენაზე წერენ ალგორითმის განმარტებას.
+x = int(input("enter your first number here "))
+y = int(input("enter your second number here "))
+jami = x + y
+print(jami)
+# pseudo code:
+# start
+# display "enter your first number here" input
+# display "enter your second number here" input
+# display jami
+# natural language:
+# გამოსახე "enter your first number here" და შემდეგ "enter your second number here" და გამოსახე მათი ჯამი.
