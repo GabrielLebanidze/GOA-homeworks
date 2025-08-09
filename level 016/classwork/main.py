73ee10db1936637b232466366a159b171bf3e168
GabrielLebanidze <gabriellebanidze@gmail.com>
2025-08-09 15:56:43 +0400

    fifteenth commit

---
+++ level 016/classwork/main.py
@@ -0,0 +1,17 @@
+age = int(input("enter your age here: "))
+if age < 18:
+    print("you are kid")
+else:
+    print("you are adult")
+
+
+password = int(input("enter your password here: "))
+if password == 1234:
+    print("password is correct")
+else:
+    print("password is incorrect")
+
+
+number = int(input("enter your number here: "))
+if number >= 0:
+    print("the number is positive")
