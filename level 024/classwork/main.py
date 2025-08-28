6998eba212def0a51341dd67d8170febc7aef6d8
GabrielLebanidze <gabriellebanidze@gmail.com>
2025-08-28 13:29:21 +0400

    twenty-second commit

---
+++ level 024/classwork/main.py
@@ -0,0 +1,11 @@
+# cars = ["lambo", "bugatti", "toyota", "BMW", "porsche"]
+# print(cars)
+# print(cars[2])
+# print(cars[3])
+
+names = ["gabriel", "giorgi", "inga", "nini", "nia", "dato", "tazo"]
+names[1], names[5] = names[2], names[3]
+print(names)
+
+names[1], names[4] = names[4], names[1]
+print(names)
