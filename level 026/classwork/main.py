ffa08c311e38af1ae8e0dbe321c676abec3d9d0d
GabrielLebanidze <gabriellebanidze@gmail.com>
2025-09-02 16:21:45 +0400

    twenty-fourth commit

---
+++ level 026/classwork/main.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5]
+print(numbers[1:3])
+
+numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers1[::2])
+print(numbers1[2:6])
+print(numbers1[2:-2:3])
+print(numbers1[::-1])
