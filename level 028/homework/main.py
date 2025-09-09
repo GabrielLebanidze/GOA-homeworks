f17b4013bd90d8c431927e7b4d50d74cd19e7bc5
GabrielLebanidze <gabriellebanidze@gmail.com>
2025-09-09 14:21:29 +0400

    twenty-sixth commit

---
+++ level 028/homework/main.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 4, 5]
+for i in numbers:
+    if i % 2 == 0:
+        print("luwi ricxvia: " + str(i))
+    else:
+        print("kenti ricxvia: " + str(i))
+
+numbers1 = [1, 2, 3, 4, 5]
+for i in numbers:
+    print(i)
+
+numbers2 = [1, 2, 3, 4]
+for i in numbers2:
+    print(i*2)
