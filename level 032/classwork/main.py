a09ee1b7eca1f138070675503f5c6a54f1d19e50
GabrielLebanidze <gabriellebanidze@gmail.com>
2025-09-16 20:34:06 +0400

    twenty-ninth commit

---
+++ level 032/classwork/main.py
@@ -0,0 +1,19 @@
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(len(list))
+
+# name = "gabrieli"
+# print(len(name))
+
+# list.append(11)
+# print(list)
+# list.append(12)
+# print(list)
+# print(len(list))
+
+numbers = ["gabriel", "giorgi", "salome", "inga", "kote"]
+numbers.insert(4, "lena")
+numbers.insert(0, "shelly")
+print(numbers)
+
+numbers.pop()
+numbers.pop(2)
