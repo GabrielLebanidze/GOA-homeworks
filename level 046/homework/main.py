1cb3086e3017aeadc3206d52d6be36a61c681afe
GabrielLebanidze <gabriellebanidze@gmail.com>
2026-01-17 13:34:40 +0400

    forty-fourth commit

--- level 046/homework/main.py
+++ level 046/homework/main.py
@@ -1,38 +1,46 @@
-# def idk():
-#     nothing = 0
-#     list = []
-#     for i in range(1, 100):
-#         if i % 3 == 0 and i % 5 == 0:
-#             nothing += 1
-#         elif i % 3 == 0 and i % 5 != 0:
-#             list.append(i)
-#         elif i % 3 != 0 and i % 5 == 0:
-#             list.append(i)
-#     return list
+def idk():
+    nothing = 0
+    list = []
+    for i in range(1, 100):
+        if i % 3 == 0 and i % 5 == 0:
+            nothing += 1
+        elif i % 3 == 0 and i % 5 != 0:
+            list.append(i)
+        elif i % 3 != 0 and i % 5 == 0:
+            list.append(i)
+    return list
 
-# print(idk())
+print(idk())
 
+def primechecker(n):
+    if n <=1:
+        return False
+    for i in range(2, int(n**0.5+1)):
+        if n % i == 0:
+            return False
+    return True
 
+def primes(n):
+    sia = []
+    for i in range(1, n+1):
+        if primechecker(i) == True:
+            sia.append(i)
+    return sia
 
-def martivichecker(N):
-    shedegi = []
-    for i in range(1, N+1):
-        if 
+print(primes(100))
 
+kenti = 0
+luwi = 0
 
+for i in range(50, 150):
+    if i % 2 == 0:
+        luwi += i
+    elif i % 2 != 0:
+        kenti += i
+print(kenti)
+print(luwi)
 
-# kenti = 0
-# luwi = 0
+def sia(g, b, r, t, y):
+    return [g, b, r, t, y]
 
-# for i in range(50, 150):
-#     if i % 2 == 0:
-#         luwi += i
-#     elif i % 2 != 0:
-#         kenti += i
-# print(kenti)
-# print(luwi)
-
-# def sia(g, b, r, t, y):
-#     return [g, b, r, t, y]
-
-# print(sia(1, 2, 3, 4, 5))
+print(sia(1, 2, 3, 4, 5))
